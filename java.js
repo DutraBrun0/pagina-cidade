3b0deabc5352e2ce932e88855c2737a19a24fcc2
Bruno Dutra <156865461+DutraBrun0@users.noreply.github.com>
2024-07-31 09:24:26 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+function carimagem(){
+    document.getElementById("conteudo").innerHTML ="<P>Entre o encantamento das torres da Sagrada Família e as formas mágicas do Parque Güell, Barcelona é um verdadeiro museu ao ar livre de Gaudí.</P>";
+      document.getElementById("imagem").innerHTML = "<img src='imagem1.jpg'>"
+
+}
+
+function carimagem2(){
+     document.getElementById("conteudo").innerHTML ="<P>La Rambla é o coração pulsante de Barcelona, onde a energia da cidade se encontra com o charme dos artistas de rua e cafés vibrantes.</P>";
+      document.getElementById("imagem").innerHTML = "<img src='imagem2.jpg'>"
+
+}
+
+function carimagem3(){
+    document.getElementById("conteudo").innerHTML ="<P>Descubra a rica tapeçaria cultural e gastronômica de Barcelona nos mercados como La Boqueria e nas obras de arte que embelezam a cidade.</P>";
+     document.getElementById("imagem").innerHTML = "<img src='imagem3.jpg'>"
+
+}
+
+function carimagem4(){
+    document.getElementById("conteudo").innerHTML ="<P>Do sol radiante à areia dourada da Praia de Barceloneta, Barcelona é o lugar perfeito para relaxar e revigorar sob o brilho do Mediterrâneo.</P>";
+     document.getElementById("imagem").innerHTML = "<img src='imagem4.jpg'>"
+
+}
